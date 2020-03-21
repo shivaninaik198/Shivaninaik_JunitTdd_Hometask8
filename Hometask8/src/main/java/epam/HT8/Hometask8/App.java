c8cc95bb14ac0507568ad099537ca376c1d87e2a
shivaninaik198 <60171175+shivaninaik198@users.noreply.github.com>
2020-03-21 17:04:43 +0530

    2nd commit

---
+++ Hometask8/src/main/java/epam/HT8/Hometask8/App.java
@@ -0,0 +1,13 @@
+package epam.HT8.Hometask8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
